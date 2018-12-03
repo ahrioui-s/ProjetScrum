a7368bbc4c066d5a08f7055fefb074077b44e806
Zaim Hamza <45354452+Hamzaim1@users.noreply.github.com>
2018-12-03 16:39:38 +0100

    commit

--- src/ConvertPdf2Txt.bat
+++ src/ConvertPdf2Txt.bat
@@ -4,7 +4,13 @@ mkdir -p info/
 for i in *.pdf
 do
 	echo $i
+	echo le nom de fichier est: $i >> "info/$i.txt"
+	
 	pdf2txt -o "text/$i.txt" "$i"
+	
+	echo Le titre de la recherche est: >> "info1/$i.txt"
 	sed -n '1,2p' "text1/$i.txt" >> "info1/$i.txt"
+	
+	echo L'abstract de l'auteur est: >> "info1/$i.txt"
 	sed -n '/Abstract/,/Introduction/p' "text2/$i.txt" >> "info/$i.txt"
 done
