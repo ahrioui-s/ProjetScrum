31e02c34e64593947147ee2c6a6ea9575bdee404
Zaim Hamza <45354452+Hamzaim1@users.noreply.github.com>
2018-12-03 16:19:11 +0100

    modif de fichier pour récupérer les données
    Récupération du nom de fichier, du titre et de l'abstract

--- src/ConvertPdf2Txt.bat
+++ src/ConvertPdf2Txt.bat
@@ -3,4 +3,6 @@ for i in *.pdf
 do
 	echo $i
 	pdf2txt -o "text/$i.txt" "$i"
+	sed -n '1,2p' "text1/$i.txt" >> "info1/$i.txt"
+	sed -n '/Abstract/,/Introduction/p' "text2/$i.txt" >> "info/$i.txt"
 done
