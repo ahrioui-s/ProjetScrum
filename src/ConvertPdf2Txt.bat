ddf63ecaf9dc2c5e53bbfbe4c4c751b7a9c5711e
Zaim Hamza <45354452+Hamzaim1@users.noreply.github.com>
2018-12-03 16:55:53 +0100

    modif sur le programme

--- src/ConvertPdf2Txt.bat
+++ src/ConvertPdf2Txt.bat
@@ -1,6 +1,6 @@
 cd "/home/nas02a/etudiants/inf/uapv1900224/Donnees_itinerantes/Papers"
-mkdir -p text2/ 
-mkdir -p info/ 
+mkdir -p text1/ 
+mkdir -p info1/ 
 for i in *.pdf
 do
 	echo $i
@@ -12,5 +12,5 @@ do
 	sed -n '1,2p' "text1/$i.txt" >> "info1/$i.txt"
 	
 	echo L'abstract de l'auteur est: >> "info1/$i.txt"
-	sed -n '/Abstract/,/Introduction/p' "text2/$i.txt" >> "info/$i.txt"
+	cat "text1/$i.txt" | sed -n '/Abstract/,/Introduction/p' | sed -e  's:.*<Abstract>::' | sed -e 's:<Introduction>.*::'  >> "info1/$i.txt"
 done
