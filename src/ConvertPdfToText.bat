6d70bff2a8befee70d33fcd38fd7faa378a49573
Zaim Hamza <45354452+Hamzaim1@users.noreply.github.com>
2018-12-03 16:20:38 +0100

    Modif de programme de convert et d'extraction
    Exctration de nom de fichier, du titre, et de l'abstract.

--- src/ConvertPdfToText.bat
+++ src/ConvertPdfToText.bat
@@ -1,6 +1,10 @@
 cd "/home/nas02a/etudiants/inf/uapv1900224/Donnees_itinerantes/Papers"
+mkdir -p text2/ 
+mkdir -p info/ 
 for i in *.pdf
 do
 	echo $i
-	pdftotext "$i" "text2/$i.txt"
+	pdftotext -f 1 -l 2 "$i" "text2/$i.txt"
+	sed -n '1,2p' "text2/$i.txt" >> "info/$i.txt"
+	sed -n '/Abstract/,/Introduction/p' "text2/$i.txt" >> "info/$i.txt"
 done
